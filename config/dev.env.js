020e50989efd0d707bf9737f00f66a34702d411c
lcg0124 <7G9v8384>
2018-07-26 12:16:25 +0800

    修改后端默认地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"http://10.30.1.172:8002"',
+  API_ROOT: '"http://localhost:8002"',
 })
